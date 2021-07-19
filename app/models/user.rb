13ecb8d99b5b8515f28ac8d448deffe748ddc8b9
helman101 <helmanandresortegon@gmail.com>
2021-07-19 12:28:06 +0200

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_one_attached :photo
   has_one_attached :coverImage
+  has_secure_password
+
+  validates :password, length: { minimum: 6 }
+  validates_uniqueness_of :email, :userName
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_precense_of :fullName, :userName, :email, :password, :password_confirmation
+
 end
