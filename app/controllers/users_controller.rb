6bd47aa24b23e96321dde135df4f7c986fa1464a
helman101 <helmanandresortegon@gmail.com>
2021-07-20 17:37:07 +0200

    generate user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+  before_action :set_user
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :photo, :coverImage, :fullName)
+  end
+
+  def set_user
+    @user = User.find(session[:user_id])
+  end
+end
