e5ec6480b170068ae0b1c68ff795262824f4d983
helman101 <helmanandresortegon@gmail.com>
2021-07-21 16:31:49 +0200

    add dependent: destroy to associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_one_attached :photo
-  has_one_attached :coverImage
-  has_many :opinions
-  has_many :followings
+  has_one_attached :photo, dependent: :destroy
+  has_one_attached :coverImage, dependent: :destroy
+  has_many :opinions, dependent: :destroy
+  has_many :followings, dependent: :destroy
   has_many :followers, through: :followings
   has_many :followed, through: :followings
   has_secure_password
