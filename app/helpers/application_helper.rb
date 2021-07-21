0230f96598ffae02871d42ea7bf1e1db16018855
helman101 <helmanandresortegon@gmail.com>
2021-07-21 15:01:58 +0200

    add user_loged? helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,12 @@ module ApplicationHelper
     !session[:current_user].nil?
   end
 
+  def user_loged?
+    if session[:current_user].nil?
+      redirect_to root_path, alert: 'You must be loged to have access'
+    end
+  end
+
   def set_user
     @user ||= User.find(session[:user_id])
   end
