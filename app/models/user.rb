d78eba3c8b61641a6a53df68fe87fd67eb128af2
helman101 <helmanandresortegon@gmail.com>
2021-07-23 16:06:00 +0200

    fix type errors, add foreign keys to associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_one_attached :photo, dependent: :destroy
   has_one_attached :coverImage, dependent: :destroy
-  has_many :opinions, dependent: :destroy
-  has_many :followings, dependent: :destroy
+  has_many :opinions, foreign_key: 'author_id', dependent: :destroy
+  has_many :followings, foreign_key: 'follower_id', dependent: :destroy
   has_many :followers, through: :followings
   has_many :followed, through: :followings
   has_secure_password
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }
   validates_uniqueness_of :email, :userName
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates_precense_of :fullName, :userName, :email, :password, :password_confirmation
+  validates_presence_of :fullName, :userName, :email, :password, :password_confirmation
 
   def friends_and_own_opinions
     Opinions.where(user: (followed.to_a << self))
