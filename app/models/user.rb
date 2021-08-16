bd8367dd46d34b74d222af9257fd4c97baa81592
helman101 <helmanandresortegon@gmail.com>
2021-08-16 17:59:08 +0200

    refactor user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_one_attached :photo, dependent: :destroy
   has_one_attached :coverImage, dependent: :destroy
   has_many :opinions, foreign_key: 'author_id', dependent: :destroy
-  has_many :followings, foreign_key: 'follower_id', dependent: :destroy
-  has_many :followers, through: :followings
-  has_many :followed, through: :followings
+  has_many :followers, class_name: 'Following', foreign_key: 'follower_id'
+  has_many :followed, class_name: 'Following', foreign_key: 'followed_id'
   has_secure_password
 
   validates :password, length: { minimum: 6 }
@@ -13,6 +12,6 @@ class User < ApplicationRecord
   validates_presence_of :fullName, :userName, :email, :password, :password_confirmation
 
   def friends_and_own_opinions
-    Opinions.where(user: (followed.to_a << self))
+    Opinion.where(user: (followed.to_a << self))
   end
 end
